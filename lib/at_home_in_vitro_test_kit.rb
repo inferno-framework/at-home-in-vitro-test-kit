64b2ee6be752030c385e140f8a57c0c098ca8f45
Dylan Hall <dehall@mitre.org>
2024-05-01 14:33:17 -0400

    FI-2429: Migrate to HL7 validator

--- lib/at_home_in_vitro_test_kit.rb
+++ lib/at_home_in_vitro_test_kit.rb
@@ -8,11 +8,13 @@ module AtHomeInVitroTestKit
     description 'An Inferno Test Kit used for validating At-Home In-Vitro FHIR Bundles and their entry resources.'
 
     # All FHIR validation requsets will use this FHIR validator
-    validator do
+    fhir_resource_validator do
+      url ENV.fetch('FHIR_RESOURCE_VALIDATOR_URL', 'http://hl7_validator_service:3500')
 
-      url ENV.fetch('VALIDATOR_URL', 'http://validator_service:4567')
+      igs 'hl7.fhir.us.home-lab-report#1.0.0'
 
       exclude_message do |message|
+        message.message.match?(/\A\S+: \S+: URL value '.*' does not resolve/) ||
         (message.type == 'warning' &&
         message.message.match?(/Global Profile reference .* could not be resolved, so has not been checked/)) || 
         (message.type == 'info' && message.message.match?(/.* This element does not match any known slice defined in the profile .*/))
